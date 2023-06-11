f0b06f7d760a4766925cf3ec30cc76e8f834744f
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-06-11 14:47:34 +0530

    mobile view modified

---
+++ script.js
@@ -0,0 +1,48 @@
+function highlightBox(j, f){
+    if (j == 1){
+        f.classList.remove("inputEmail");
+        f.classList.add("inputEmailII");
+    }else{
+        f.classList.add("inputEmail");
+        f.classList.remove("inputEmailII");
+    }
+}
+
+function enterKey(event) {
+    if (event.keyCode === 13){
+        subscribe();
+        return;
+    }
+}
+
+function subscribe() {
+    const e = document.querySelector("#emailInput");
+    let email = e.value;
+    const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailIsCorrect = emailFormat.test(email);
+    if (emailIsCorrect) {
+        e.classList.remove("inputEmailIII");
+        e.classList.add("inputEmail");
+        window.location.href = 'thankyoupage.html';
+    } else {
+        e.classList.remove("inputEmail");
+        e.classList.add("inputEmailIII");
+        document.querySelector(".errorMsg").style.visibility = "visible";
+    }
+}
+
+function dismiss() {
+    window.location.href = 'index.html';
+}
+
+function changeColor(i, e) {
+    if (i == 1) {
+        e.style.backgroundColor = "hsl(4, 100%, 67%)";
+        e.style.borderColor = "hsl(4, 100%, 67%)";
+        e.style.boxShadow = "2px 3px 16px hsl(4, 100%, 67%)";
+    } else {
+        e.style.backgroundColor = "hsl(234, 29%, 20%)";
+        e.style.borderColor = "hsl(234, 29%, 20%)";
+        e.style.boxShadow = "none";
+    }
+}
