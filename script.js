cc892c3beb90416a2cf3a10d861043f4e1b6fbde
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2024-05-10 23:38:19 +0530

    desktop design and code completed

---
+++ script.js
@@ -0,0 +1,37 @@
+$('#emailSubmit').click(function () {
+    const input = ($('#emailInput').val()).toString() ? ($('#emailInput').val()).toString() : false;
+    if (displayError(input)) {
+        if (/[a-zA-Z]+[0-9]?@+/.test(input)) {
+            localStorage.setItem('email', input);
+            $('.loaderDiv').css('opacity', 1);
+            setTimeout(()=>{
+                $('.loaderDiv').css('opacity', 0);
+                window.location.href = "success.html";
+            }, 1000);
+        } else {
+            displayError(false);
+        }
+    }
+});
+
+function displayError(err) {
+    if (!err) {
+        $('.errorLabel').css('opacity', 1);
+        $('#emailInput').css({
+            "border-color": $(':root').css('--error-input-border'),
+            "background-color": $(':root').css('--error-input-bg'),
+            "color": $(':root').css('--error-input-color')
+        });
+        return false;
+    } else {
+        if ($('.errorLabel').css('opacity') == 1) {
+            $('.errorLabel').css('opacity', 0);
+            $('#emailInput').css({
+                "border-color": $(':root').css('--normal-input-border'),
+                "background-color": $(':root').css('--normal-input-bg'),
+                "color": $(':root').css('--normal-input-color')
+            });
+        }
+        return true;
+    }
+}
